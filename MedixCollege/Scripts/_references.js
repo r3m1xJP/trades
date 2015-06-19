04a6f6f9b31b6a3a3dd89fd9371c4af80a0a5a22
Johnson Phanyasaeng <jphanyasaeng@gmail.com>
2015-06-19 18:39:39 -0400

    added youtube popup javascript refactored caption class for responsive slides changed london map from medix to trades address

--- MedixCollege/Scripts/_references.js
+++ MedixCollege/Scripts/_references.js
@@ -13,3 +13,4 @@
 /// <reference path="npm.js" />
 /// <reference path="bootstrap.min.js" />
 /// <reference path="responsiveslides.min.js" />
+/// <reference path="bootstrap.youtubepopup.min.js" />
